64f7914c3acf1713598c9ffd24207c677317b374
YuriyChernyshov <chernyshov.yuriy@gmail.com>
2016-01-22 22:25:04 -0500

    Change the year from 2015 to 2016

--- app/src/main/java/com/yuriy/openradio/api/StreamVO.java
+++ app/src/main/java/com/yuriy/openradio/api/StreamVO.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 The "Open Radio" Project. Author: Chernyshov Yuriy
+ * Copyright 2016 The "Open Radio" Project. Author: Chernyshov Yuriy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
