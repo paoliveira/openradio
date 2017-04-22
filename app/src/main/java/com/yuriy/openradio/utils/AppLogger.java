85b38762fa166f2fa9c904356b75d233b1f77b4e
Yurii Chernyshov <chernyshov.yuriy@gmail.com>
2017-04-22 18:46:19 -0400

    Update Copyright.

--- app/src/main/java/com/yuriy/openradio/utils/AppLogger.java
+++ app/src/main/java/com/yuriy/openradio/utils/AppLogger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 The "Open Radio" Project. Author: Chernyshov Yuriy [chernyshov.yuriy@gmail.com]
+ * Copyright 2017 The "Open Radio" Project. Author: Chernyshov Yuriy [chernyshov.yuriy@gmail.com]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
